d14851f26535c1ef2cd7f49148e6e57c7df98712
Luis Theml <44423391+Theml@users.noreply.github.com>
2023-05-22 15:55:53 -0300

    Update Home.jsx

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -2,9 +2,13 @@ import './Home.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Footer from '../../layout/footer/Footer';
 import Principal from '../../layout/main/Principal';
+import { useAuth } from '../../components/AuthContext/AuthContext';
 
 
 const Home = () => {
+
+    const {user} = useAuth()
+
     return(
         <div className='container-fluid d-flex flex-column' style={{"height": "90vh"}}>
             {/* <div className='row'>
@@ -13,7 +17,10 @@ const Home = () => {
 
             <div className='row flex-grow-1 d-flex g-0'>
                 <div className='col-12'>
+                    {!user?
                     <Principal titulo="TELA HOME"/>
+                    :
+                    <Principal titulo={"Bem vindo \n" + user.nome}/>}
                 </div>
             </div>
 
